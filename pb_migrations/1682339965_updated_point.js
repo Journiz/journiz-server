e7f8d0351cea8e293de2197f40f244950a3d115e
leoboyerbx <contact@leoboyer.fr>
2023-04-24 14:39:40 +0200

    fix required fields in point

---
+++ pb_migrations/1682339965_updated_point.js
@@ -0,0 +1,71 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("x5qckklqadwyz2k")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "fr7lstv4",
+    "name": "name",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "dmenh55p",
+    "name": "question",
+    "type": "text",
+    "required": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("x5qckklqadwyz2k")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "fr7lstv4",
+    "name": "name",
+    "type": "text",
+    "required": true,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "dmenh55p",
+    "name": "question",
+    "type": "text",
+    "required": true,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
