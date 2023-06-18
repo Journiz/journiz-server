af1a9c63016a573bd1b5945d06c70ad26bc4fccc
leoboyerbx <contact@leoboyer.fr>
2023-06-18 12:03:43 +0200

    update point model for hasLocation

---
+++ pb_migrations/1687082604_updated_point.js
@@ -0,0 +1,25 @@
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("x5qckklqadwyz2k")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "2wh94ssn",
+    "name": "hasLocation",
+    "type": "bool",
+    "required": false,
+    "unique": false,
+    "options": {}
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("x5qckklqadwyz2k")
+
+  // remove
+  collection.schema.removeField("2wh94ssn")
+
+  return dao.saveCollection(collection)
+})
